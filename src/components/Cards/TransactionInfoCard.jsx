7b0a39f141ed88d8f098fbc02909c5d341904e1d
HarsH <jharsh0002@gmail.com>
2025-07-05 11:55:32 +0530

    fixed delete button for phones

--- src/components/Cards/TransactionInfoCard.jsx
+++ src/components/Cards/TransactionInfoCard.jsx
@@ -38,7 +38,7 @@ const TransactionInfoCard = ({
         <div className='flex items-center gap-2'>
           {!hideDeleteBtn && (
             <button
-              className='text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer'
+              className='text-gray-400 hover:text-red-500 opacity-100 md:opacity-0  group-hover:md:opacity-100 transition-opacity cursor-pointer'
               onClick={onDelete}
             >
               <LuTrash2 size={18} />
